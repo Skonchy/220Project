ce45d58508906307067116c929c572ecfc707282
unknown <j.jessup16@gmail.com>
2018-12-03 22:29:51 -0600

    Adds imports and removes extension

--- src/Salukify/Main.java
+++ src/Salukify/Main.java
@@ -1,12 +1,16 @@
 package Salukify;
-public class Main extends Library {
+
+import Salukify.Queue;
+import Salukify.Library;
+
+public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Library lib = new Library("Library");
 		System.out.println(lib.root.getLabel());
-		lib.root.addChild(new Node("Metal"));
-		lib.root.addChild(new Node("Rock"));
+		lib.root.addChild(new Library.Node("Metal"));
+		lib.root.addChild(new Library.Node("Rock"));
 		
 		lib.root.getChildren();
 
