10d1a85a4acadc7ee0fdbb7a9c4b9d2b0d8ab015
Skonchy <mrskonchy@gmail.com>
2018-12-05 00:30:15 -0600

    replaced the menu so that methods can be referenced

--- src/Salukify/Main.java
+++ src/Salukify/Main.java
@@ -9,79 +9,78 @@ import java.util.Scanner;
 
 public class Main {
 	static String tempSong;
-	static Scanner scan = new Scanner(System.in);
+	
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 		// TODO Auto-generated method stub
 		Library lib = new Library("Library");
 		System.out.println(lib.root.getLabel());
 		lib.addChild(new Library.Node("METAL"));
 		lib.addChild(new Library.Node("ROCK"));
 		
+		/*lib.add("test song");
+		lib.add("2nd test song");
+		
 		System.out.println(lib.root);
 		System.out.println(((Node) lib.root.getChildren().elementAt(1)).getParent());
-		lib.root.printChildren();
-		//menu();
-		lib.explore();
+		System.out.println(lib.root.printChildren());
 		
-
+		lib.search("metal");
+		lib.root.removeChild("metal");*/
+		
+		int choice = menu();
+		while(choice != 0) {
+			switch(choice) {
+			case 1:
+				System.out.println("Please enter the name of the song you want to find. 0 to exit.");
+					String song = scan.next();
+					if(!song.equalsIgnoreCase("0")) {
+						lib.search(song);
+					}else {
+						System.exit(0);
+					}
+			case 2:
+				System.out.println("Please enter the name of the song you would like to add to the Library.");
+				String add = scan.nextLine();
+				if(!add.equalsIgnoreCase("0")) {
+					lib.add(add);
+				}else {
+					System.exit(0);
+				}
+				break;
+			case 3:
+				System.out.println("Please enter the name of the song you would like to remove from the Library. 0 to exit.");
+				String rem = scan.nextLine();
+				if(!rem.equalsIgnoreCase("0")) {
+					//lib.remove(rem);
+				}else {
+					System.exit(0);
+				}
+				break;
+			case 4:
+				lib.explore();
+				break;
+			default: 
+				System.out.println("Please make a selection within the bounds");
+				break;
+			}
+			System.exit(0);
+		}
 	}
 	
-	public static void menu() {
+	public static int menu() {
+		int selection;
+		Scanner input = new Scanner(System.in);
 		System.out.println("Salukify Menu: ");
 		System.out.println("--------------------");
 		System.out.println("1) Search for song");
 		System.out.println("2) Add a song"); 
 		System.out.println("3) Remove a song");
 		System.out.println("4) Explore");
-		System.out.println("Please enter a number: 0 exits");
-
-		//Needs a scanner for user input!!
-		String choice = scan.next();
-		while(choice != "0") {
-			switch(choice) {
-			case "0":
-				System.out.println("Thank you for using Salukify");
-				System.exit(0);
-				break;
-			case "1":
-				//Search
-				System.out.println("What song would you like to search for?");
-				//Set user input to tempSong;
-				String tempSong = scan.nextLine();
-				//Use search method
-				//DECISION REQUIRED:  might return path or boolean
-				break;
-			
-			case "2" :
-				//Add
-				//lib.add()
-				/*if(root.children.elementAt(i) == scan,next()) {
-					Node currentNode = root.children.elementAt(i)
-				}*/
-				//ask for all necessary info to step through 
-				// adds non-existing nodes to the vector list of that level
-				// enter that node
-				//  steps down and adds next info until done
-				//NOTE: toUpperCase so no repeats?
-				
-				break;
-			case "3":
-				//Remove
-				System.out.println("What song would you like to remove from the library?");
-				//User input set to tempSong; --This should be taken care of in the nesting
-				//search for existence of song Note: use search method?
-				//remove the song
-				//remove(search(scan.next()));
-				
-				break;
-			case "4":
-				//Exploring
-				//Basically lib.root.getChildren();
-				break;
-				
-			}
+		System.out.println("0) Exit");
+		selection = input.nextInt();
+		return selection;
 		}
-	}
 
 }
